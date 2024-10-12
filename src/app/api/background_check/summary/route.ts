c3d9d07d7038360abbc8aa2084291589ca7fdebb
PyDevelopment <nachoblancasrodriguez@gmail.com>
2024-10-12 19:56:13 -0400

    more api endpoints

--- src/app/api/background_check/summary/route.ts
+++ src/app/api/background_check/summary/route.ts
@@ -1,3 +1,7 @@
+import OpenAI from "openai";
+import { zodResponseFormat } from "openai/helpers/zod";
+import { z } from "zod";
+
 export async function POST(req: Request) {
     try {
         const { news, ...res } = await req.json();
