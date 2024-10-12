7d2975b43b73ca979943a9023340fd090b4d9264
Alex Khosrowshahi <alexander_khosrowshahi@brown.edu>
2024-10-12 16:03:36 -0400

    fix: The big one

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Input } from "@/app/components"
+import { useState } from "react"
+
+export default function Home() {
+  const [value, setValue] = useState<string>("")
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    const response = await fetch("https://localhost:3000/api/NACHOSAPI", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "url": value
+      }),
+    })
+    const data = await r
+  }
+
+  return (
+    <main className="flex flex-col justify-start items-center w-full pt-8">
+      <div className="flex items-center justify-center py-4">
+        <img src="/pawpaw.png"></img>
+        <p>Pawpaw is the most best and awesome way of protecting your rights!</p>
+      </div>
+      
+      <form className="max-w-[700px] min-w-[700px]" onSubmit={(e) => onSubmit(e)}>
+        <Input value={value} setValue={setValue} />
+      </form>
+    </main>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        tertiary: "var(--tertiary)",
+        accent: "var(--accent)",
       },
     },
   },
