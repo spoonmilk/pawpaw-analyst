4595b3c5aed870883ff7596243bc5a5f525c74f9
Alex Khosrowshahi <alexander_khosrowshahi@brown.edu>
2024-10-12 23:20:04 -0400

    feat: added a bunch of frontend stuff

---
+++ src/app/components/HoverPopup.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+type ClassNamesProps = {
+  wrapper: string;
+  parent: string;
+  hover: string;
+};
+
+export default function HoverPopup({
+  classNames = { wrapper: "", hover: "", parent: "" },
+  hoverElement,
+  children,
+}: {
+  classNames?: ClassNamesProps;
+  hoverElement: JSX.Element;
+  children: React.ReactNode;
+}) {
+  const [isHovered, setIsHovered] = React.useState<boolean>(false);
+
+  const handleMouseOver = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div className={`relative ${classNames.wrapper}`}>
+      <div
+        className={`hover-card-trigger ${classNames.parent}`}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        {children}
+      </div>
+      <AnimatePresence>
+        {isHovered && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.5 }}
+            transition={{ type: "spring", stiffness: 100, damping: 20 }}
+            className={`hover-card absolute bg-white border-gray-600 rounded p-4 shadow-2xl ${classNames.hover}`}
+          >
+            {hoverElement}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,35 @@
 'use client';
 
 import { DisplayText, Input, LoadingIcon } from "@/app/components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Data, KeyPoints } from "@/app/lib/types/Types";
 import JumpButtons from "./components/JumpButtons";
+import Image from "next/image";
+import harold from "./public/haroldface.svg";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const [value, setValue] = useState<string>("")
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<Data | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    }
+  }, [error]);
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
+
+    if (value === "") {
+      setError("Please enter a URL");
+      return;
+    }
+
     try {
       setLoading(true);
   
@@ -25,6 +43,7 @@ export default function Home() {
         }),
         cache: "no-cache",
       });
+
       const originalTextPromise = fetch(`http://localhost:3000/api/search/original_text`, {
         method: "POST",
         headers: {
@@ -38,6 +57,13 @@ export default function Home() {
       
       const [keyPointsResponse, originalTextResponse] = await Promise.all([keyPointsPromise, originalTextPromise]);
   
+      if (!keyPointsResponse.ok || !originalTextResponse.ok) {
+        setValue("");
+        setError("An error occurred. Please try again later.");
+        setLoading(false);
+        return;
+      }
+      
       const keyPointsData = await keyPointsResponse.json() as KeyPoints;
       const originalTextData = await originalTextResponse.json() as { original_text: string };
   
@@ -50,7 +76,9 @@ export default function Home() {
       setValue("");
       setLoading(false);
     } catch (error) {
-      setValue("");
+      setValue("")
+      setError("An error occurred. Please try again later.");
+      setLoading(false);
       console.error(error);
     }     
   }
@@ -59,11 +87,31 @@ export default function Home() {
     <main className="flex flex-col justify-start items-center w-full px-8 overflow-x-hidden">
       
       <div className="flex items-center justify-center py-4">
-        <p>Pawpaw is the most best and awesome way of protecting your rights!</p>
+        <Image src={harold} alt="harold" className="h-80 w-auto"/>
+        <div className="flex flex-col items-center justify-center">
+        <h1>Welcome to PAWPAW!</h1>
+          <p>
+            <span className="block">Privacy Analyis With Personality And Whimsy (PAWPAW) is a tool that allows users</span>
+            <span className="block">to analyze EULAs, SLAs, TOSs, and other legal documents to pinpoint</span>
+            <span className="block"> how companies may be misusing your data. </span> 
+          </p>
+        </div>
       </div>
 
       <form className="w-[80%] max-w-[700px] py-8" onSubmit={(e) => onSubmit(e)}>
         <Input value={value} setValue={setValue} />
+        {error ? 
+          <motion.p 
+            className="text-red-500 font-bold text-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            {error}
+          </motion.p>
+          : <></>
+        }
       </form>
 
       <LoadingIcon isLoading={loading} />
